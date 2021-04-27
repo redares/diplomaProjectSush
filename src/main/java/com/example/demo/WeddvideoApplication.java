c00f51468e6fa49c2336bf63161d65ac6714b930
redares <lodroft@gmail.com>
2021-04-27 13:58:48 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/WeddvideoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddvideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddvideoApplication.class, args);
+	}
+
+}
