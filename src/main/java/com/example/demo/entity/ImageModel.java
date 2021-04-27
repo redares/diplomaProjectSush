856c6156dccb142b5603d1b0dc2c75955e75e377
redares <lodroft@gmail.com>
2021-04-27 16:10:05 +0300

    filled in the entity

--- src/main/java/com/example/demo/entity/ImageModel.java
+++ src/main/java/com/example/demo/entity/ImageModel.java
@@ -1,4 +1,12 @@
 package com.example.demo.entity;
 
 public class ImageModel {
+
+    private Long id;
+    private String name;
+    private byte[] imageBytes;
+    private Long userId;
+    private Long postId;
+
+
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,13 +1,11 @@
 package com.example.demo.entity;
 
 import com.example.demo.entity.enums.ERole;
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.PrePersist;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class User {
 
@@ -22,6 +20,11 @@ public class User {
     private Set<ERole> role = new HashSet<>();
     private List<Post> posts = new ArrayList<>();
     private LocalDateTime createdDate;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User() {
+    }
 
     @PrePersist
     protected void onCreate(){
