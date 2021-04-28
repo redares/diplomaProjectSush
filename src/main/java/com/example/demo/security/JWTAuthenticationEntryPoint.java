68270b152783fceef5ffa623f247bc8292f6ed32
redares <lodroft@gmail.com>
2021-04-28 10:24:33 +0300

    add JWTAuthenticationEntryPoint

---
+++ src/main/java/com/example/demo/security/JWTAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package com.example.demo.security;
+
+import com.example.demo.payload.response.InvalidLoginResponse;
+import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//ловим ошибку авторизации и выдаем ошибку 401 (неавторизирован), когда пользователь
+//пытается получить защищенный ресурс без авторизации
+@Component
+public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        InvalidLoginResponse loginResponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(loginResponse);
+        httpServletResponse.setContentType(SecurityConstants.CONTENT_TYPE);
+        httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+        httpServletResponse.getWriter().println(jsonLoginResponse);
+    }
+}
